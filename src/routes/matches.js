598dfc540bd95a2d9daff65907ec42bb8ac1b771
Aman Kumar95 <amansd349@gmail.com>
2026-07-18 18:05:37 +0530

    fetching matches info from the database

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,16 +1,39 @@
 import { Router } from "express";
-import { createMatchSchema } from "../validation/matches.js";
+import { createMatchSchema, listMatchesQuerySchema } from "../validation/matches.js";
 import { json } from "zod";
 import { matches } from "../db/schema.js";
 import { getMatchStatus } from "../utils/match-status.js";
+import { db } from "../db/db.js";
+import { desc } from "drizzle-orm";
 
 export const matchRouter = Router();
+const MAX_LIMIT= 100;
+matchRouter.get("/", async(req, res) => {
+ const parsed= listMatchesQuerySchema.safeParse(req.query)
 
-matchRouter.get("/", (req, res) => {
-  res.status(200).json({ message: "Matches List" });
+ if (!parsed.success) {
+    return res.status(400).json({error:'Invalid query',details:JSON.stringify(parsed.error)})
+
+ }
+
+ const limit= Math.min(parsed.data.limit?? 50,MAX_LIMIT)
+
+try {
+    const data= await db
+                    .select()
+                    .from(matches)
+                    .orderBy(desc(matches.createdAt))
+                    .limit(limit)
+
+res.json({data});
+
+} catch (e) {
+    res.status(500).json({error: 'Failed to list matches.'})
+}
+
 });
 
-matchRouter.post("/", (req, res) => {
+matchRouter.post("/", async(req, res) => {
   const parsed = createMatchSchema.safeParse(req.body);
     const {data:{startTime,endTime,homeScore,awayScore}}= parsed;
   if (!parsed.success) {
@@ -23,7 +46,7 @@ matchRouter.post("/", (req, res) => {
   }
 
   try {
-    const[event]= await db.insert(matches).values({
+    const [event]= await db.insert(matches).values({
         ...parsed.data,
         startTime: new Date(startTime),
         endTime:new Date(endTime),
@@ -34,6 +57,8 @@ matchRouter.post("/", (req, res) => {
     
     res.status(201).json({data:event})
   } catch (e) {
+    console.log(e);;
+    
     res
       .status(500)
       .json({ error: "Failed to create match", details: JSON.stringify(e) });
